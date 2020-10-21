c78a411f2d7871a2c07751c99b6c3cf2307f61c5
pezzu <7243796+pezzu@users.noreply.github.com>
2020-10-21 00:53:32 -0400

    body-parser is removed (not needed since express 4.16+)

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const path = require("path");
 
 const routes = require("./routes");
@@ -7,8 +6,7 @@ const routes = require("./routes");
 const app = express();
 const port = process.env.PORT || 8090;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use("/api", routes.routes());
 
