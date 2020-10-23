656b7f82e2785a67357f8fdef1d47c0ccf1a8b9a
pezzu <7243796+pezzu@users.noreply.github.com>
2020-10-22 23:43:41 -0400

    Fix static serve path

--- src/server/server.js
+++ src/server/server.js
@@ -12,9 +12,9 @@ app.use("/api", routes.routes());
 
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.resolve(__dirname, "../build"))); // Handle React routing, return all requests to React app
+  app.use(express.static(path.resolve(__dirname, "../../build"))); // Handle React routing, return all requests to React app
   app.get("*", (req, res) => {
-    res.sendFile(path.jresolve(__dirname, "../build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../../build", "index.html"));
   });
 }
 
